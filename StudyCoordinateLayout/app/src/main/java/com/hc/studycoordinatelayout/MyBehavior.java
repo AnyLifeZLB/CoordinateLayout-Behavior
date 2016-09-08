8347666d2b695fa6c8855e9658ed748de46507ce
B5FCAA07.曾刘保 <zenglb@vanke.net.cn>
2016-09-08 17:30:20 +0800

    添加一些注释啊神的

--- StudyCoordinateLayout/app/src/main/java/com/hc/studycoordinatelayout/MyBehavior.java
+++ StudyCoordinateLayout/app/src/main/java/com/hc/studycoordinatelayout/MyBehavior.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 /**
- * Package com.hc.studycoordinatelayout
+ *
  * Created by HuaChao on 2016/6/1.
  */
 public class MyBehavior extends CoordinatorLayout.Behavior<Button> {
@@ -20,6 +20,14 @@ public class MyBehavior extends CoordinatorLayout.Behavior<Button> {
         width = display.widthPixels;
     }
 
+	/**
+     *
+     *
+     * @param parent
+     * @param child        其实Child是指要执行动作的CoordinatorLayout的子View
+     * @param dependency   Dependency是指Child依赖的View
+     * @return
+     */
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, Button child, View dependency) {
         //如果dependency是TempView的实例，说明它就是我们所需要的Dependency
@@ -29,7 +37,6 @@ public class MyBehavior extends CoordinatorLayout.Behavior<Button> {
     //每次dependency位置发生变化，都会执行onDependentViewChanged方法
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, Button btn, View dependency) {
-
         //根据dependency的位置，设置Button的位置
 
         int top = dependency.getTop();
@@ -38,7 +45,7 @@ public class MyBehavior extends CoordinatorLayout.Behavior<Button> {
         int x = width - left - btn.getWidth();
         int y = top;
 
-        setPosition(btn, x, y);
+        setPosition(btn, x, y);  //根据Dependency 的位置设置Child 的位置
         return true;
     }
 
